608a48f94072247959b089f8b97c5dd116cf33c0
Kfir1 <kfirrp@gmail.com>
2023-11-17 21:48:42 +0200

    rendering comments by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -5,11 +5,20 @@ import React from "react";
 export default ({ comments }) => {
   // map over that state and produce a list of comments
   // display them to user
-
   const renderedComments =
     comments.length > 0 ? (
       comments.map((comment) => {
-        return <li key={comment.id}>{comment.content}</li>;
+        let content;
+        if (comment.status === "approved") {
+          content = comment.content;
+        }
+        if (comment.status === "pending") {
+          content = "Comment is awaiting moderation";
+        }
+        if (comment.status === "rejected") {
+          content = "Comment has been rejected";
+        }
+        return <li key={comment.id}>{content}</li>;
       })
     ) : (
       <span></span>
